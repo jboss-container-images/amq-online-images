5edb1bf0f7d6da5c444f5e7dc5a8ac940b0b0613
Vanessa Busch <vbusch@redhat.com>
2017-12-04 00:52:07 -0500

    corrected JBOSS_HOME path
    cleaned up debugging info.  Used variable JBOSS_HOME

--- auth-server/modules/auth.install/install.sh
+++ auth-server/modules/auth.install/install.sh
@@ -6,11 +6,16 @@ SOURCES_DIR=/tmp/artifacts/
 
 DISTRO_NAME=keycloak-bin
 
+SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
+
 
 # unpack
 {
 	tar xzf "${SOURCES_DIR}/$DISTRO_NAME.tar.gz" -C $JBOSS_HOME
+	mv ${ADDED_DIR}/addSaslPlugin.xsl $JBOSS_HOME
+	chown jboss:0 $JBOSS_HOME/addSaslPlugin.xsl
 }
-
-$JBOSS_HOME/bin/init-keycloak.sh
+chmod ug+x ${ADDED_DIR}/init-keycloak.sh
+${ADDED_DIR}/init-keycloak.sh
 
