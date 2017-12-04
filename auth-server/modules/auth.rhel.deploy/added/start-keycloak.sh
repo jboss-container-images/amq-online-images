42376c4cb93b0ee7dc446c5161ff04be59681fe4
Vanessa Busch <vbusch@redhat.com>
2017-12-04 00:52:07 -0500

    corrected JBOSS_HOME path
    cleaned up debugging info.  Used variable JBOSS_HOME

--- auth-server/modules/auth.rhel.deploy/added/start-keycloak.sh
+++ auth-server/modules/auth.rhel.deploy/added/start-keycloak.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-KEYSTORE_PATH=/opt/eap/standalone/configuration/certificates.keystore
-CERT_PATH=/opt/jboss/keycloak/standalone/cert
+KEYSTORE_PATH=${JBOSS_HOME}/standalone/configuration/certificates.keystore
+CERT_PATH=${JBOSS_HOME}/standalone/cert
 
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     /opt/eap/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
-    echo 'loaded user'
 fi
 
 openssl pkcs12 -export -passout pass:enmasse -in ${CERT_PATH}/tls.crt -inkey ${CERT_PATH}/tls.key -name "server" -out /tmp/certificates-keystore.p12
 
 keytool -importkeystore -srcstorepass enmasse -deststorepass enmasse -destkeystore ${KEYSTORE_PATH} -srckeystore /tmp/certificates-keystore.p12 -srcstoretype PKCS12
 
-/opt/eap/bin/standalone.sh -b 0.0.0.0 -c standalone.xml
+${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 -c standalone.xml
