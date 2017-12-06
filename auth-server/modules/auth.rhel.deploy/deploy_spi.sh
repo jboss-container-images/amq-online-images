cfd633b89e05ced9d026ff07a5102392604851ea
Vanessa Busch <vbusch@redhat.com>
2017-12-05 22:24:47 -0500

    Corrected auth-server/image_rhel.yaml scripts to deploy the spi provider. Cleaned up concreate scripts: - modified version to 1.1 for openjdk and rhel - removed references to unused repos - removed the image_yaml file for the rpm scripts

--- auth-server/modules/auth.rhel.deploy/deploy_spi.sh
+++ auth-server/modules/auth.rhel.deploy/deploy_spi.sh
@@ -6,8 +6,11 @@ SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
 mv ${ADDED_DIR}/start-keycloak.sh $JBOSS_HOME/bin
+mv ${ADDED_DIR}/addSpiProvider.xsl $JBOSS_HOME
+
 chmod -R ug+x $JBOSS_HOME/bin/start-keycloak.sh
 chown jboss:0 $JBOSS_HOME/bin/start-keycloak.sh
 
-cat $JBOSS_HOME/bin/start-keycloak.sh
-mv ${JBOSS_HOME}/providers/sasl-plugin*.jar ${JBOSS_HOME}/standalone/deployments/sasl-plugin.jar
+KEYCLOAK_CONFIG=${JBOSS_HOME}/standalone/configuration/standalone.xml
+java -jar /usr/share/java/saxon.jar -s:${KEYCLOAK_CONFIG} -xsl:${JBOSS_HOME}/addSpiProvider.xsl -o:${KEYCLOAK_CONFIG}
+
