9f8bd216f096b6abc4a507c5b129a055ee6d6af7
Vanessa Busch <vbusch@redhat.com>
2017-12-06 19:56:32 -0500

    Removed dependency on saxon which isn't available

--- auth-server/modules/auth.install/install.sh
+++ auth-server/modules/auth.install/install.sh
@@ -14,7 +14,11 @@ ADDED_DIR=${SCRIPT_DIR}/added
 {
 	tar xzf "${SOURCES_DIR}/$DISTRO_NAME.tar.gz" -C $JBOSS_HOME
 	mv ${ADDED_DIR}/addSaslPlugin.xsl $JBOSS_HOME
+	mv ${ADDED_DIR}/removeFileLogging.xsl $JBOSS_HOME
+	mv ${ADDED_DIR}/addKeyStore.xsl $JBOSS_HOME
+	chown jboss:0 $JBOSS_HOME/addKeyStore.xsl
 	chown jboss:0 $JBOSS_HOME/addSaslPlugin.xsl
+	chown jboss:0 $JBOSS_HOME/removeFileLogging.xsl
 }
 chmod ug+x ${ADDED_DIR}/init-keycloak.sh
 ${ADDED_DIR}/init-keycloak.sh
--- auth-server/modules/auth.rhel.deploy/deploy_spi.sh
+++ auth-server/modules/auth.rhel.deploy/deploy_spi.sh
@@ -7,10 +7,9 @@ ADDED_DIR=${SCRIPT_DIR}/added
 
 mv ${ADDED_DIR}/start-keycloak.sh $JBOSS_HOME/bin
 mv ${ADDED_DIR}/addSpiProvider.xsl $JBOSS_HOME
+chown jboss:0 $JBOSS_HOME/addSpiProvider.xsl
 
+chmod ug+x ${ADDED_DIR}/init-keycloak-spiprovider.sh
+${ADDED_DIR}/init-keycloak-spiprovider.sh
 chmod -R ug+x $JBOSS_HOME/bin/start-keycloak.sh
 chown jboss:0 $JBOSS_HOME/bin/start-keycloak.sh
-
-KEYCLOAK_CONFIG=${JBOSS_HOME}/standalone/configuration/standalone.xml
-java -jar /usr/share/java/saxon.jar -s:${KEYCLOAK_CONFIG} -xsl:${JBOSS_HOME}/addSpiProvider.xsl -o:${KEYCLOAK_CONFIG}
-
