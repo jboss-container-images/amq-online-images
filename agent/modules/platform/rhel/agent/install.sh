c754780aee497df8485e114935d18262a3569e5c
Vanessa Busch <vbusch@redhat.com>
2018-03-13 15:49:48 -0400

    updated to to use 0.17.1 release.

--- agent/modules/platform/rhel/agent/install.sh
+++ agent/modules/platform/rhel/agent/install.sh
@@ -5,17 +5,17 @@ set -e
 SOURCES_DIR=/tmp/artifacts/
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
-
+DIST_NAME="nodejs-amqmaas-agent-1.0.0-8"
 {
 	cd ${SOURCES_DIR}
 	rpm -ih -r "${SOURCES_DIR}" "${SOURCES_DIR}/nodejs-amqmaas-agent-zip.rpm"
 	mkdir -p /opt/app-root/src/
-	unzip /tmp/artifacts/usr/share/doc/nodejs-amqmaas-agent-1.0.0-7.zip -d /opt/app-root/src/
+	unzip "/tmp/artifacts/usr/share/doc/${DIST_NAME}.zip" -d /opt/app-root/src/
 }
 
 
 {
-	cd /opt/app-root/src/nodejs-amqmaas-agent-1.0.0-7
+	cd "/opt/app-root/src/${DIST_NAME}"
 	mv node_modules/agent/* /opt/app-root/src
 	mv node_modules /opt/app-root/src
 	mv "${ADDED_DIR}/launch_node.sh" /opt/app-root/src/bin/
