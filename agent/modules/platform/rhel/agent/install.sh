3ec4fd4b2fe70566be122ce43a287da12c2db6da
Vanessa Busch <vbusch@redhat.com>
2018-04-25 21:02:53 -0400

    upgraded agent to 0.19
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- agent/modules/platform/rhel/agent/install.sh
+++ agent/modules/platform/rhel/agent/install.sh
@@ -2,24 +2,20 @@
 
 set -e
 
-SOURCES_DIR=/tmp/artifacts/
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
-DIST_NAME="nodejs-amqmaas-agent-1.0.0-9"
+DIST_NAME="nodejs-amqmaas-agent-1.0.0-*"
 {
-	cd ${SOURCES_DIR}
-	rpm -ih -r "${SOURCES_DIR}" "${SOURCES_DIR}/nodejs-amqmaas-agent-zip.rpm"
 	mkdir -p /opt/app-root/src/
-	unzip "/tmp/artifacts/usr/share/doc/${DIST_NAME}.zip" -d /opt/app-root/src/
+	unzip "/usr/share/${DIST_NAME}.zip" -d /opt/app-root/src/
 }
 
-
 {
-	cd "/opt/app-root/src/${DIST_NAME}"
+	cd /opt/app-root/src/${DIST_NAME}
 	mv node_modules/agent/* /opt/app-root/src
 	mv node_modules /opt/app-root/src
 	mv "${ADDED_DIR}/launch_node.sh" /opt/app-root/src/bin/
 }
 
 groupadd -r jboss -g 185 && useradd -u 185 -r -g jboss -m -d /home/jboss -s /sbin/nologin -c "JBoss user" jboss
-chown -R 1001:root /opt/app-root
+chown -R jboss:root /opt/app-root
