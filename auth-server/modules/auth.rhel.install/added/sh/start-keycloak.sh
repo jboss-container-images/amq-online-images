879d189eef73ce101300ed34ea3ddbcfe8306c8b
Ulf Lilleengen <lulf@redhat.com>
2018-06-11 11:38:18 +0200

    Add new resources for SSO 72 deployment

--- auth-server/modules/auth.rhel.install/added/sh/start-keycloak.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
-    /opt/eap/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
-fi
-
-${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 -c standalone.xml
