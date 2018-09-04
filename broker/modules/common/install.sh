0b270b16bc1d0e70e87fd734aa2e6416d07f9e81
Keith Wall <kwall@apache.org>
2018-09-04 15:02:50 +0100

    Fix-up broker configuration to match upstream where appropiate.

--- broker/modules/common/install.sh
+++ broker/modules/common/install.sh
@@ -11,19 +11,19 @@ DISTRO_NAME=apache-artemis
 # unpack apache-artemis
 {
 	tar xzf "${SOURCES_DIR}/$DISTRO_NAME-bin.tar.gz" -C /opt
-	if [ ! -d /opt/apache-artemis-2.4.0 ]; then
-		mv /opt/$DISTRO_NAME* /opt/apache-artemis-2.4.0
+	if [ ! -d /opt/apache-artemis-2.6.1 ]; then
+		mv /opt/$DISTRO_NAME* /opt/apache-artemis-2.6.1
 	fi
 	
 	mkdir -p /shutdown-hook
-#	mkdir -p /opt/apache-artemis-2.4.0/bin/launch.sh
+#	mkdir -p /opt/apache-artemis-2.6.1/bin/launch.sh
 	mv "${ADDED_DIR}/config_templates" /
 	mv ${ADDED_DIR}/shutdown-hook.sh /shutdown-hook/shutdown-hook.sh	
-	mv ${ADDED_DIR}/launch.sh /opt/apache-artemis-2.4.0/bin/launch.sh
-	mv ${ADDED_DIR}/probe.sh /opt/apache-artemis-2.4.0/bin/probe.sh
+	mv ${ADDED_DIR}/launch.sh /opt/apache-artemis-2.6.1/bin/launch.sh
+	mv ${ADDED_DIR}/probe.sh /opt/apache-artemis-2.6.1/bin/probe.sh
 
-    mv ${SOURCES_DIR}/amqp-connector.jar /opt/apache-artemis-2.4.0/lib/amqp-connector.jar
-    mv ${SOURCES_DIR}/sasl-delegation.jar /opt/apache-artemis-2.4.0/lib/sasl-delegation.jar
+    mv ${SOURCES_DIR}/amqp-connector.jar /opt/apache-artemis-2.6.1/lib/amqp-connector.jar
+    mv ${SOURCES_DIR}/sasl-delegation.jar /opt/apache-artemis-2.6.1/lib/sasl-delegation.jar
 
     mkdir -p /jmx_exporter
     mv ${SOURCES_DIR}/jmx-exporter.jar /jmx_exporter/jmx-exporter.jar
@@ -36,5 +36,5 @@ chown 185 /var/run/artemis
 chown -R 185:0 /home/jboss
 chmod 0755 /home/jboss
 chmod -R g+rwX /home/jboss
-chown -R 185:0 /opt/apache-artemis-2.4.0/bin
-chmod 0755 /opt/apache-artemis-2.4.0/bin/*
+chown -R 185:0 /opt/apache-artemis-2.6.1/bin
+chmod 0755 /opt/apache-artemis-2.6.1/bin/*
