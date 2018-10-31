3d9bc7c4f13161b4c06f134e72feb0b73aeae3fb
Ulf Lilleengen <lulf@redhat.com>
2018-10-31 22:57:15 +0100

    Add plans and infra configs

--- address-space-controller/modules/common/install.sh
+++ address-space-controller/modules/common/install.sh
@@ -3,6 +3,8 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
+SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
 
 DISTRO_NAME=address-space-controller
 
@@ -10,4 +12,8 @@ DISTRO_NAME=address-space-controller
 # unpack
 {
     mv "$SOURCES_DIR/address-space-controller.jar" /opt
+    mv "$ADDED_DIR/addressplans" /
+    mv "$ADDED_DIR/addressspaceplans" /
+    mv "$ADDED_DIR/brokeredinfraconfigs" /
+    mv "$ADDED_DIR/standardinfraconfigs" /
 }
