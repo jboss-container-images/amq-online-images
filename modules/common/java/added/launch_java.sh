80d86065fce3a3d90475f8354173bb77c6f9f324
Ulf Lilleengen <lulf@redhat.com>
2018-11-01 15:32:17 +0100

    Set log level for java images

--- modules/common/java/added/launch_java.sh
+++ modules/common/java/added/launch_java.sh
@@ -7,7 +7,9 @@ source /usr/local/dynamic-resources/dynamic_resources.sh
 
 JAVA_OPTS="$(/opt/run-java/java-default-options)"
 
+LOG_LEVEL=${LOG_LEVEL:-info}
+
 # Make sure that we use /dev/urandom
-JAVA_OPTS="${JAVA_OPTS} -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
+JAVA_OPTS="${JAVA_OPTS} -DLOG_LEVEL=${LOG_LEVEL} -Dvertx.disableDnsResolver=true -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
 
 exec java $JAVA_OPTS -jar $JAR $JAVA_OPTS $@
