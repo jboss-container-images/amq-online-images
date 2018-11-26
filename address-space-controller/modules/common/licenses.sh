9bc55ad2b8af62da90cca9a899bbf7ba7e1ab7f0
Ulf Lilleengen <lulf@pvv.ntnu.no>
2018-11-26 12:34:13 +0100

    Use address space controller bundle (#87)
    * Use address space controller bundle artifact
    
    * Fix artifact name

--- address-space-controller/modules/common/licenses.sh
+++ address-space-controller/modules/common/licenses.sh
@@ -6,6 +6,7 @@ PRODUCT_IDENTIFIER=$(echo ${JBOSS_IMAGE_NAME} | awk -F'/' '{print $2}')
 LICENSE_DIR=/root/licenses/${PRODUCT_IDENTIFIER}
 SOURCES_DIR=/tmp/artifacts/
 
-mkdir -p ${LICENSE_DIR}
-unzip -nj "$SOURCES_DIR/address-space-controller.jar" 'licenses/*' -d ${LICENSE_DIR}
+unzip "$SOURCES_DIR/address-space-controller.zip" -d ${SOURCES_DIR}/address-space-controller
 
+mkdir -p ${LICENSE_DIR}
+unzip -nj "$SOURCES_DIR/address-space-controller/address-space-controller.jar" 'licenses/*' -d ${LICENSE_DIR}
