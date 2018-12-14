6ed8246ca8a75a12ef91e613007b3829617b011a
Vanessa <vbusch@redhat.com>
2018-12-14 02:33:27 -0500

    reuse scripts from upstream project
    - reusing init-broker.sh and launch-broker.sh
    
    Signed-off-by: Vanessa <vbusch@redhat.com>

--- broker-plugin/modules/broker-plugin-installer/install.sh
+++ broker-plugin/modules/broker-plugin-installer/install.sh
@@ -4,7 +4,6 @@ set -e
 
 SOURCES_DIR=/tmp/artifacts/
 SCRIPT_DIR=$(dirname $0)
-ADDED_DIR=${SCRIPT_DIR}/added
 BROKER_PLUGIN_DIR=/opt/broker-plugin
 
 {
@@ -14,14 +13,10 @@ BROKER_PLUGIN_DIR=/opt/broker-plugin
 
 	unzip "${SOURCES_DIR}/broker-plugin.zip" -d /
 
-    mv ${ADDED_DIR}/init-broker.sh ${BROKER_PLUGIN_DIR}/bin/init-broker.sh
-    mv ${ADDED_DIR}/launch-broker.sh ${BROKER_PLUGIN_DIR}/bin/launch-broker.sh
- 
-    mv ${SOURCES_DIR}/jmx-exporter.jar ${BROKER_PLUGIN_DIR}/jmx_exporter/jmx-exporter.jar
+    mv ${SOURCES_DIR}/jmx-exporter.jar ${BROKER_PLUGIN_DIR}/jmx_exporter/
 
     mv ${SOURCES_DIR}/netty-tcnative-boringssl-static.jar ${BROKER_PLUGIN_DIR}/lib/
     mv ${SOURCES_DIR}/netty-tcnative-boringssl-static-linux-x86_64.jar ${BROKER_PLUGIN_DIR}/lib/
-
 }
 
 chown -R 185:0 /home/jboss
