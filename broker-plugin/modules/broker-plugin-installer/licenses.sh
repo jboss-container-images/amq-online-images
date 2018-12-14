47ee0ffbefa9881a7c0c1840e0f84ad5e0d8ee01
vbusch <vbusch@redhat.com>
2018-12-14 08:08:30 -0500

    reuse scripts from upstream project (#95)
    - reusing init-broker.sh and launch-broker.sh
    
    Signed-off-by: Vanessa <vbusch@redhat.com>

--- broker-plugin/modules/broker-plugin-installer/licenses.sh
+++ broker-plugin/modules/broker-plugin-installer/licenses.sh
@@ -6,7 +6,7 @@ PRODUCT_IDENTIFIER=$(echo ${JBOSS_IMAGE_NAME} | awk -F'/' '{print $2}')
 SOURCES_DIR=/tmp/artifacts/
 unzip "$SOURCES_DIR/broker-plugin.zip" -d ${SOURCES_DIR}/broker-plugin
 
-for jar in opt/broker-plugin/lib/amqp-connector.jar opt/broker-plugin/lib/sasl-delegation.jar opt/shutdown-hook/lib/shutdown-hook.jar
+for jar in opt/broker-plugin/lib/amqp-connector.jar opt/broker-plugin/lib/sasl-delegation.jar opt/broker-plugin/shutdown-hook/lib/shutdown-hook.jar
 do
     LICENSE_DIR=/root/licenses/${PRODUCT_IDENTIFIER}/${jar}
     mkdir -p ${LICENSE_DIR}
