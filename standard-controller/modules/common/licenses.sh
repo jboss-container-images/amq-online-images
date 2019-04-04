607b7f6514b599a106e5580b74220ccc22bd436b
Ulf Lilleengen <lulf@redhat.com>
2019-04-04 21:30:01 +0200

    Fetch zip for standard-controller

--- standard-controller/modules/common/licenses.sh
+++ standard-controller/modules/common/licenses.sh
@@ -6,6 +6,8 @@ PRODUCT_IDENTIFIER=$(echo ${JBOSS_IMAGE_NAME} | awk -F'/' '{print $2}')
 LICENSE_DIR=/root/licenses/${PRODUCT_IDENTIFIER}
 SOURCES_DIR=/tmp/artifacts/
 
+unzip "$SOURCES_DIR/standard-controller.zip" -d ${SOURCES_DIR}/standard-controller
+
 mkdir -p ${LICENSE_DIR}
-unzip -nj "$SOURCES_DIR/standard-controller.jar" 'licenses/*' -d ${LICENSE_DIR}
+unzip -nj "$SOURCES_DIR/standard-controller/standard-controller.jar" 'licenses/*' -d ${LICENSE_DIR}
 
