311d4cbd27711b826a895a573ec9896ed3f9a171
k-wall <kwall@apache.org>
2019-09-10 09:09:19 +0100

    install yq (and jq) asd part of OSBS pipeline (#157)

--- scripts/build-images-virtual-env.sh
+++ scripts/build-images-virtual-env.sh
@@ -4,9 +4,18 @@
 
 virtualenv ~/cekit
 source ~/cekit/bin/activate
+
+if ! which jq
+then
+    LOCAL_JQ_BIN=~/cekit/bin/jq
+    curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output "${LOCAL_JQ_BIN}"
+    chmod a+x "${LOCAL_JQ_BIN}"
+fi
+
 pip install odcs
 pip install docker-py
 pip install docker-squash
+pip install yq
 
 pip install -U cekit
 make
