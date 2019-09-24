950303bed119eb0f503ddbac9f4953b228d8df4e
Ulf Lilleengen <lulf@redhat.com>
2019-09-24 11:20:06 +0200

    Update cekit path

--- scripts/build-images-virtual-env.sh
+++ scripts/build-images-virtual-env.sh
@@ -2,12 +2,12 @@
 
 #sudo pip3 install virtualenv
 
-virtualenv ~/cekit
-source ~/cekit/bin/activate
+virtualenv ~/cekit3
+source ~/cekit3/bin/activate
 
 if ! which jq
 then
-    LOCAL_JQ_BIN=~/cekit/bin/jq
+    LOCAL_JQ_BIN=~/cekit3/bin/jq
     curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output "${LOCAL_JQ_BIN}"
     chmod a+x "${LOCAL_JQ_BIN}"
 fi
