311b6ffab197816258d537b9ad96229d63ff3d91
Keith Wall <kwall@apache.org>
2019-10-22 13:24:54 +0100

    Await the outcome of the image build tasks

--- scripts/push-image-from-brew.sh
+++ scripts/push-image-from-brew.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SUBDIR_START_LINE=`grep -n -m 1 "$(pwd)" ${CEKIT_OUTPUT_FILE} | cut -f1 -d: `;
-TOTAL_LINES=`wc -l ${CEKIT_OUTPUT_FILE} | cut -f1 -d ' '`;
-LINE_TO_TAIL=`expr ${TOTAL_LINES} - ${SUBDIR_START_LINE}`;
-URL=`tail -${LINE_TO_TAIL} ${CEKIT_OUTPUT_FILE} | egrep -e "${OSBS_URL}/brew/taskinfo\?taskID=[0-9]*" -o -m 1`;
+URL=`cat ${CEKIT_LOG} | egrep -e "${OSBS_URL}/brew/taskinfo\?taskID=[0-9]*" -o -m 1`;
 
 if [ -z "${URL}" ] ; then
 	echo "No taskinfo for ${pwd}";
