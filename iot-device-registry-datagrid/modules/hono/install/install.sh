4050d74fac60701c042e4fb663e672eb02b3fee5
Ulf Lilleengen <lulf@redhat.com>
2019-11-14 13:38:08 +0100

    Add install and licences for each iot module

---
+++ iot-device-registry-datagrid/modules/hono/install/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCES_DIR=/tmp/artifacts/
+
+mv "$SOURCES_DIR/iot-device-registry-infinispan.jar" /opt
---
+++ iot-tenant-service/modules/hono/ip/licenses.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+PRODUCT_IDENTIFIER=$(echo ${JBOSS_IMAGE_NAME} | awk -F'/' '{print $2}')
+LICENSE_DIR=/root/licenses/${PRODUCT_IDENTIFIER}
+SOURCES_DIR=/tmp/artifacts/
+TMP=/tmp
+
+mkdir -p "${LICENSE_DIR}"
+unzip -nj "${SOURCES_DIR}/iot-tenant-service.jar" 'BOOT-INF/classes/licenses/*' -d ${LICENSE_DIR}
