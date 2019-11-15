8ce2dc1df3c2dfb16d4d6965e3dd69cebac46ed9
Ulf Lilleengen <lulf@redhat.com>
2019-11-15 12:17:31 +0100

    Fix install script

--- iot-proxy-configurator/modules/install-artifact/install.sh
+++ iot-proxy-configurator/modules/install-artifact/install.sh
@@ -6,8 +6,4 @@ SOURCES_DIR=/tmp/artifacts/
 
 # unpack
 mkdir -p /opt/
-for zip in $(find "${SOURCES_DIR}" -name "*.zip")
-do
-    unzip ${zip} -d /opt
-done
 unzip ${SOURCES_DIR}/iot-proxy-configurator-dist.zip -d /opt
