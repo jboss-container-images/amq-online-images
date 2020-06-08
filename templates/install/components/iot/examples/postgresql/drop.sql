b00aed6c40020010f4dc26a67cfb1e4f29bbd452
AMQ Online CI <amq-online-ci@redhat.com>
2020-06-08 07:10:12 -0400

    Update templates

---
+++ templates/install/components/iot/examples/postgresql/drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE device_registrations;
+DROP TABLE device_credentials;
+
+DROP INDEX idx_device_registrations_tenant;
+DROP INDEX idx_device_credentials_tenant;
+DROP INDEX idx_device_credentials_tenant_auth_id;
