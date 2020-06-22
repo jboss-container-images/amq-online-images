6205fe0e9e2417c0803a7e6cd8a920e677717414
Jens Reimann <jreimann@redhat.com>
2020-06-22 10:20:59 +0200

    Fix location of JAR files (#197)

--- iot-device-registry/modules/hono/install/install.sh
+++ iot-device-registry/modules/hono/install/install.sh
@@ -3,7 +3,8 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
+TARGET_DIR="/"
 
-mv "$SOURCES_DIR/iot-device-connection-infinispan.jar" /opt
-mv "$SOURCES_DIR/iot-device-registry-infinispan.jar" /opt
-mv "$SOURCES_DIR/iot-device-registry-jdbc.jar" /opt
+mv "$SOURCES_DIR/iot-device-connection-infinispan.jar" "$TARGET_DIR"
+mv "$SOURCES_DIR/iot-device-registry-infinispan.jar" "$TARGET_DIR"
+mv "$SOURCES_DIR/iot-device-registry-jdbc.jar" "$TARGET_DIR"
