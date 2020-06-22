77fb6ab85b1fbd7d19ba745be09d3dae0a695b8a
Jens Reimann <jreimann@redhat.com>
2020-06-22 09:38:51 +0200

    Fix location of JAR files

--- iot-adapters/modules/hono/install/install.sh
+++ iot-adapters/modules/hono/install/install.sh
@@ -3,8 +3,9 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
+TARGET_DIR=/
 
-mv "$SOURCES_DIR/iot-http-adapter.jar" /opt
-mv "$SOURCES_DIR/iot-lorawan-adapter.jar" /opt
-mv "$SOURCES_DIR/iot-mqtt-adapter.jar" /opt
-mv "$SOURCES_DIR/iot-sigfox-adapter.jar" /opt
+mv "$SOURCES_DIR/iot-http-adapter.jar" "$TARGET_DIR"
+mv "$SOURCES_DIR/iot-lorawan-adapter.jar" "$TARGET_DIR"
+mv "$SOURCES_DIR/iot-mqtt-adapter.jar" "$TARGET_DIR"
+mv "$SOURCES_DIR/iot-sigfox-adapter.jar" "$TARGET_DIR"
